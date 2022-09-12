033c42fa04a5415b72d280a8bb7bf9d2403f1c28
risma261 <syafeirisma@students.unnes.ac.id>
2022-09-12 14:43:07 +0700

    approve tagih

--- app/Http/Livewire/RekapSubkon.php
+++ app/Http/Livewire/RekapSubkon.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Livewire;
 
-use Livewire\Component;
+use Carbon\Carbon;
 use App\Models\Resub;
+use Livewire\Component;
+use Illuminate\Support\Facades\Redirect;
 
 class RekapSubkon extends Component
 {
     public $sortBy = 'created_at';
     public $sortDirection = 'desc';
     public $selectedStatus ='';
+    public $checkedTagih =[];
     public $searchColumnsKode, $searchColumnsNama, $searchColumnsPriceMin, $searchColumnsPriceMax, $searchColumnsCategoryId;
 
     public function sortBy($columnName)
@@ -27,8 +30,9 @@ class RekapSubkon extends Component
         $columns = ['Kode Barang','Nama Barang'];
         $items = Resub::query()
                     ->orderBy($this->sortBy, $this->sortDirection)
+                    ->where('status_tagih',0)
                     ->paginate();
-                    
+
         return view('livewire.rekap-subkon',[
             'title' => 'Tagihan Subcon',
             'ket' => 'Tabel ',
@@ -36,5 +40,16 @@ class RekapSubkon extends Component
             'icon' => 'groups',
             'columns'=>$columns
         ]);
+    }
+
+    public function Approve(){
+        Resub::query()
+            ->whereIn('id', $this->checkedTagih)
+            ->update([
+                    'tgl_tagih' => Carbon::now(),
+                    'status_tagih' => 1,
+        ]);
+
+            return Redirect::back();
     }
 }
