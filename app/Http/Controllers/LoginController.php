5e3b130c45477e6f9f062d6f0cfebb3284218cc6
Achmat Sodikkun <75171774+sodkun@users.noreply.github.com>
2022-10-24 19:20:32 +0700

    fix bug logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -20,6 +20,6 @@ class LoginController extends Controller
 
     public function logout () {
         Auth::logout();
-        return redirect("/");
+        return redirect("http://erp.alluresystem.site");
     }
 }
